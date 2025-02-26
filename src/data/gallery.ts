73e46c9e355c9f1cd7f2328f7904fcdbb89c07a3
Oswin Jerome <oswinjeromej@gmail.com>
2025-02-26 11:02:20 +0530

    v1

---
+++ src/data/gallery.ts
@@ -0,0 +1,27 @@
+import logo from "@/assets/logo.png";
+import p1 from "@/assets/gallery/p1.jpg";
+import p2 from "@/assets/gallery/p2.jpg";
+import p3 from "@/assets/gallery/p3.jpg";
+import p4 from "@/assets/gallery/p4.jpg";
+export const GALLERY_IMAGES = [
+  {
+    image: p1,
+    description: "",
+  },
+  {
+    image: p2,
+    description: "",
+  },
+  {
+    image: p3,
+    description: "",
+  },
+  {
+    image: p4,
+    description: "",
+  },
+  {
+    image: p3,
+    description: "",
+  },
+];
---
+++ src/data/solutions.ts
@@ -0,0 +1,21 @@
+import bag from "@/assets/solutions/bag.jpg";
+import pens from "@/assets/solutions/pens.jpg";
+import yum from "@/assets/solutions/yum.jpg";
+
+export const SOLUTIONS = [
+  {
+    image: bag,
+    title: "Branded Bags",
+    description: "Custom bags with your Nagercoil company logo.",
+  },
+  {
+    image: pens,
+    title: "Corporate Stationery",
+    description: "Professional stationery sets for your Nagercoil office.",
+  },
+  {
+    image: yum,
+    title: "Event Materials",
+    description: "Banners, posters, and more for your corporate events in Nagercoil.",
+  },
+];
